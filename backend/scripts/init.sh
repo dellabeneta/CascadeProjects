c631d00d2c268b5efc96ede25a406a52dc0d5526
dellabeneta <m.dellabeneta@gmail.com>
2024-11-24 21:37:51 -0300

    feat: Atualiza script de inicialização

--- backend/scripts/init.sh
+++ backend/scripts/init.sh
@@ -3,5 +3,8 @@
 # Criar usuário admin
 python scripts/create_admin_simple.py
 
+# Popular com 200 fakes de pessoas
+python scripts/create_fake_pessoas.py
+
 # Iniciar o servidor
 uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
